1b03be201584f7bb04b9a2e148ff3558b9399840
fiddlemike <fiddlemike@yahoo.com>
2015-10-24 08:56:30 -0700

    Hopefully last change to clean string

--- liquibase_mysql_scraper/src/com/curmudgeonlaureate/liquibase_mysql_scraper/ScraperUtils.java
+++ liquibase_mysql_scraper/src/com/curmudgeonlaureate/liquibase_mysql_scraper/ScraperUtils.java
@@ -82,11 +82,11 @@ public class ScraperUtils {
 	 } //end method createDirectoryStructure
 
 	 public static String cleanString(String inputString){
-		 String cleanedString = inputString.replaceAll("<=", " &le; ");
+		 String cleanedString = inputString.replaceAll("<=", " &lt;= ");
 		 cleanedString = cleanedString.replaceAll(">", " &gt; ");
-		 cleanedString = cleanedString.replaceAll(">=", " &ge; ");
-		 cleanedString = cleanedString.replaceAll("<>", " != ");
-		 cleanedString = cleanedString.replaceAll("<=>", " = ");
+		 cleanedString = cleanedString.replaceAll(">=", " &gt;= ");
+		 cleanedString = cleanedString.replaceAll("<>", " &lt;&gt; ");
+		 cleanedString = cleanedString.replaceAll("<=>", " &lt;=&gt; ");
 		 cleanedString = cleanedString.replaceAll("#", "-- ");
 		 cleanedString = cleanedString.replaceAll("<", " &lt; ");
 				
